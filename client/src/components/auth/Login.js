b7da1635a65e6dee649c9ce13f6c78588a37385e
Lxmbjs <ryan.lambacher@gmail.com>
2020-03-07 15:53:50 -0500

    Fixed login page text

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -43,7 +43,7 @@ const Login = props => {
   return (
     <div className='form-container'>
       <h1>
-        Account <span className='text-primary'>Register</span>
+        Account <span className='text-primary'>Login</span>
       </h1>
       <form onSubmit={onSubmit}>
         <div className='form-group'>
